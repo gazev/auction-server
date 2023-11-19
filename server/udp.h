057e8108f9d059002ad04d3c885936f8f39620ae
vugonz <goncalooazevedo@outlook.com>
2023-11-19 17:28:40 +0000

    add logic of response buffer and handler return values

--- server/udp.h
+++ server/udp.h
@@ -3,14 +3,14 @@
 
 #include "server.h"
 
-int serve_udp_command(char *input, struct udp_client *client);
+int serve_udp_command(char *request, struct udp_client *client, char *response, size_t *response_len);
 
-int handle_login(char *input, struct udp_client *client);
-int handle_logout(char *input, struct udp_client *client);
-int handle_unregister(char *input, struct udp_client *client);
-int handle_exit(char *input, struct udp_client *client);
-int handle_open(char *input, struct udp_client *client);
-int handle_close(char *input, struct udp_client *client);
-int handle_my_auctions(char *input, struct udp_client *client);
+int handle_login(char *req, struct udp_client *client, char *resp, size_t *resp_len);
+int handle_logout(char *req, struct udp_client *client, char *resp, size_t *resp_len);
+int handle_unregister(char *req, struct udp_client *client, char *resp, size_t *resp_len);
+int handle_exit(char *req, struct udp_client *client, char *resp, size_t *resp_len);
+int handle_open(char *req, struct udp_client *client, char *resp, size_t *resp_len);
+int handle_close(char *req, struct udp_client *client, char *resp, size_t *resp_len);
+int handle_my_auctions(char *req, struct udp_client *client, char *resp, size_t *resp_len);
 
 #endif
